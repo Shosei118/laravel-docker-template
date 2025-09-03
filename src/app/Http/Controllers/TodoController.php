14ce8d7e098ee76574c3091261cc44d3e163109c
Shose118 <syo118kerokeropain@gmail.com>
2025-09-04 00:05:38 +0900

    feat: TodoControllerで取得したtodosをview関数でbladeに渡す処理を追加

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -2,15 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use App\Todo;
 
 class TodoController extends Controller
 {
     public function index()
     {
-        $todo = new Todo(); 
+        $todo = new Todo();
         $todos = $todo->all();
         
-        return view('todo.index');
+        return view('todo.index', ['todos' => $todos]);
     }
 }
